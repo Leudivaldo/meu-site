0498242b3561b1a7fe0937f63c1d3efc83132dd7
Leudivaldo <92278553+Leudivaldo@users.noreply.github.com>
2025-11-14 18:32:28 -0300

    Delete site-teste directory
    Signed-off-by: Leudivaldo <92278553+Leudivaldo@users.noreply.github.com>

--- site-teste/scripts_1/main.js
+++
@@ -1,8 +0,0 @@
-const myHeading = document.querySelector("#dem");
-
-myHeading.addEventListener("click", function() {
-  myHeading.textContent = "Novo nome";
-  myHeading.removeEventListener("click", this);
-});
-
-

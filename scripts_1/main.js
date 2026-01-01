8bb48499737789c779ba410c2b15ff1bfa49dfcd
Leudivaldo <92278553+Leudivaldo@users.noreply.github.com>
2025-12-31 21:44:23 -0300

    Implement countdown timer for New Year's Eve 2024
    Added a countdown timer until New Year's Eve 2024.
    
    Signed-off-by: Leudivaldo <92278553+Leudivaldo@users.noreply.github.com>

--- scripts_1/main.js
+++ scripts_1/main.js
@@ -3,6 +3,18 @@ const myHeading = document.querySelector("#dem");
 myHeading.addEventListener("click", function() {
   myHeading.textContent = "Novo nome";
   myHeading.removeEventListener("click", this);
-});
+});const anoNovo = new Date('December 31, 2024 23:59:59').getTime();
+
+const x = setInterval(() => {
+  const agora = new Date().getTime();
+  const falta = anoNovo - agora;
+
+  const dias = Math.floor(falta / (1000 * 60 * 60 * 24));
+  const horas = Math.floor((falta % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutos = Math.floor((falta % (1000 * 60 * 60)) / (1000 * 60));
+  const segundos = Math.floor((falta % (1000 * 60)) / 1000);
+
+
+
 
 

2de7fe5c5156e804ca01f807e1363917eef58ecd
Leudivaldo <92278553+Leudivaldo@users.noreply.github.com>
2025-12-31 21:53:40 -0300

    Update main.js
    Signed-off-by: Leudivaldo <92278553+Leudivaldo@users.noreply.github.com>

--- scripts_1/main.js
+++ scripts_1/main.js
@@ -3,7 +3,10 @@ const myHeading = document.querySelector("#dem");
 myHeading.addEventListener("click", function() {
   myHeading.textContent = "Novo nome";
   myHeading.removeEventListener("click", this);
-});const anoNovo = new Date('December 31, 2024 23:59:59').getTime();
+});
+
+
+const anoNovo = new Date('December 31, 2024 23:59:59').getTime();
 
 const x = setInterval(() => {
   const agora = new Date().getTime();
@@ -13,6 +16,7 @@ const x = setInterval(() => {
   const horas = Math.floor((falta % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   const minutos = Math.floor((falta % (1000 * 60 * 60)) / (1000 * 60));
   const segundos = Math.floor((falta % (1000 * 60)) / 1000);
+
 
 
 
